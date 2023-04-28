65ba122e43a9f6a60623fb470120bf58e9de0a84
Joraarce <65260605+Joraarce@users.noreply.github.com>
2023-04-27 20:47:13 -0600

    Update AppData.js
    colocar class="icon icon-tabler icon-tabler-x"
    para que la multiplicación sea congruente con la suma, resta y división.

--- src/AppData.js
+++ src/AppData.js
@@ -1006,7 +1006,7 @@ const AppData = {
         name: "Multiplicar",
         symbol: (
           <svg
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg" //class="icon icon-tabler icon-tabler-x"
             viewBox="0 0 24 24"
             fill="none"
           >
